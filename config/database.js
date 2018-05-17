5bc1dfe29380a0f0b4974eb2264db6cd1c0d373b
Miguel A. Covarrubias <m.covar20@gmail.com>
2018-05-17 00:17:57 -0500

    fix

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    'url' : 'mongodb://admin:photomatic@ds053648.mlab.com:53648/photomaticmx',
-    // 'SENDGRID_SECRET': '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',
-    // 'SENDGRID_KEY': 'L69GtjcmRNOZTMb7dibvcg'
-};
